a70eb65c1413f240e5ea2fb93c89987df95eacec
Daniel Åvall <daniel@avall.net>
2023-02-11 13:25:10 +0100

    Added group by hour/day graphs

--- html/app.js
+++ html/app.js
@@ -1,9 +1,25 @@
 
 function loadData() {
 	let r = Math.floor(Math.random() * 100000);
-	$.get("bbk.log?r=" + r, function(data, status){
+	$.get("bbk.log?r=" + r, function(data, status) {
 		displayFileContent(data);
-		renderDiagram(data);
+
+		let entries = parseData(data);
+		drawLogChart(entries);
+		drawGroupByChart(
+			entries,
+			'hour-chart',
+			'Speed by hour',
+			function(entry) { return entry.timestamp.getHours(); },
+			function(entry) { return entry.timestamp.getHours(); }
+		);
+		drawGroupByChart(
+			entries,
+			'day-chart',
+			'Speed by day',
+			function(entry) { return (entry.timestamp.getDay() + 6) % 7; },
+			function(entry) { return entry.timestamp.toLocaleString('en-us', { weekday: 'long' }); }
+		);
 	});
 }
 
@@ -11,31 +27,47 @@ function displayFileContent(fileContent) {
 	document.getElementById('raw-content').innerText = fileContent;
 }
 
-function renderDiagram(fileContent) {
-	var lySerie = { name: "Latency",  yAxis: 1, color: "orange",  data: [], tooltip: {valueSuffix: ' ms'} };
-	var dlSerie = { name: "Download", yAxis: 0, color: "#008800", data: [], tooltip: {valueSuffix: ' Mbit/s'} };
-	var ulSerie = { name: "Upload",   yAxis: 0, color: "#000088", data: [], tooltip: {valueSuffix: ' Mbit/s'} };
-
-	var series = [dlSerie, ulSerie, lySerie];
-
+function parseData(fileContent) {
+	const entries = [];
 	fileContent.split('\n').forEach(function(line) {
 		if(line !== "") {
-			var parts = line.split(' ');
-			var time = new Date(parts[0]).getTime();
-
-			var latency = parseFloat(parts[1]);
-			var download = parseFloat(parts[2]);
-			var upload = parseFloat(parts[3]);
-
-			lySerie.data.push([time, latency]);
-			dlSerie.data.push([time, download]);
-			ulSerie.data.push([time, upload]);
+			const parts = line.split(' ');
+			const entry = {
+				timestamp: new Date(parts[0]),
+				latency: parseFloat(parts[1]),
+				download: parseFloat(parts[2]),
+				upload: parseFloat(parts[3])
+			}
+			entries.push(entry);
 		};
 	});
+	return entries;
+}
 
-	Highcharts.chart('container', {
+function createSeries() {
+	const dlSerie = { name: "Download", yAxis: 0, color: "#008800", data: [], tooltip: {valueSuffix: ' Mbit/s'} };
+	const ulSerie = { name: "Upload",   yAxis: 0, color: "#000088", data: [], tooltip: {valueSuffix: ' Mbit/s'} };
+	const lySerie = { name: "Latency",  yAxis: 1, color: "orange",  data: [], tooltip: {valueSuffix: ' ms'} };
+
+	const series = [dlSerie, ulSerie, lySerie];
+	return series;
+}
+
+function drawLogChart(entries) {
+	var series = createSeries();
+
+	entries.forEach(function(entry) {
+		series[0].data.push([entry.timestamp.getTime(), entry.download]);
+		series[1].data.push([entry.timestamp.getTime(), entry.upload]);
+		series[2].data.push([entry.timestamp.getTime(), entry.latency]);
+	});
+
+	Highcharts.chart('log-chart', {
 		title: {
-			text: 'Bredbandskollen - speed log'
+			text: 'Speed'
+		},
+		subtitle: {
+			text: entries.length + ' entries'
 		},
 		chart: {
 			type: 'line',
@@ -68,6 +100,101 @@ function renderDiagram(fileContent) {
 		],
 		xAxis: {
 			type: 'datetime'
+		},
+		series: series
+	});
+}
+
+function drawGroupByChart(entries, chartId, chartTitle, groupKeyFunc, groupNameFunc) {
+	// group by hour
+	let n = 0;
+	const groupObj = {};
+	entries.forEach(function(entry) {
+		if(entry.latency < 0 || entry.download < 0 || entry.upload < 0) {
+			return;
+		}
+
+		let key = groupKeyFunc(entry);
+		let group = groupObj[key];
+		if(!group) {
+			group = {
+				key: key,
+				name: groupNameFunc(entry),
+				entries: []
+			};
+			groupObj[key] = group;
+		}
+		group.entries.push(entry);
+		n++;
+	});
+	let groups = Object.values(groupObj);
+	groups.sort(function(g1, g2) {
+		return g1.key - g2.key;
+	});
+
+	// calculate average
+	groups.forEach(function(group) {
+		group.average = {
+			latency: 0,
+			download: 0,
+			upload: 0
+		};
+		group.entries.forEach(function(entry) {
+			group.average.latency += entry.latency;
+			group.average.download += entry.download;
+			group.average.upload += entry.upload;
+		});
+		group.average.latency /= group.entries.length;
+		group.average.download /= group.entries.length;
+		group.average.upload /= group.entries.length;
+	});
+
+	// create chart data
+	const categories = [];
+	const series = createSeries();
+	groups.forEach(function(group) {
+		categories.push(group.name);
+		series[0].data.push(group.average.download);
+		series[1].data.push(group.average.upload);
+		series[2].data.push(group.average.latency);
+	});
+
+	Highcharts.chart(chartId, {
+		title: {
+			text: chartTitle
+		},
+		subtitle: {
+			text: n + ' entries'
+		},
+		chart: {
+			type: 'column'
+		},
+		legend: {
+			layout: 'horizontal',
+			align: 'center',
+			verticalAlign: 'bottom'
+		},
+		tooltip: {
+			shared: true,
+			valueDecimals: 1
+		},
+		yAxis: [
+			{
+				title: {
+					text: 'Mbit/s'
+				},
+				min: 0
+			},
+			{
+				title: {
+					text: 'ms'
+				},
+				min: 0,
+				opposite: true
+			}
+		],
+		xAxis: {
+			categories: categories
 		},
 		series: series
 	});
